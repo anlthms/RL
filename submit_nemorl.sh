5b47e4e9cb93e30760c11857469a5dea61c96d7f
Anil Thomas <anlthms@users.noreply.github.com>
2026-08-04 18:13:33 -0700

    chore(experiments): point the launcher mount at this checkout
    Signed-off-by: Anil Thomas <anlthms@users.noreply.github.com>

--- submit_nemorl.sh
+++ submit_nemorl.sh
@@ -7,7 +7,7 @@ export QOS=${QOS:-interactive}
 export TIMEOUT_MIN=${TIMEOUT_MIN:-240}
 
 export CONTAINER="/lustre/fs1/portfolios/llmservice/projects/llmservice_fm_text/users/tene/nemo_rl_0624.sqsh"
-export MOUNTS="/lustre:/lustre,/home:/home,/lustre/fs1/portfolios/nemotron/projects/nemotron_sw_pre/users/anthomas/RL2:/opt/nemo-rl,${HOME}:${HOME}"
+export MOUNTS="/lustre:/lustre,/home:/home,/lustre/fs1/portfolios/nemotron/projects/nemotron_sw_pre/users/anthomas/RL3:/opt/nemo-rl,${HOME}:${HOME}"
 
 # Leave the command below commented out to get an interactive job.
 
