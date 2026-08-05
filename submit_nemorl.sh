e7c58471c93990ed8a9116348b9c7c8c975e599b
Anil Thomas <anlthms@users.noreply.github.com>
2026-08-04 19:44:35 -0700

    chore(experiments): move the launcher to the 0804 container
    The 0624 image predates the current main by ~140 commits and ~300 Megatron-LM
    commits, so its baked venvs no longer match uv.lock. Default to the 0804 image
    and let CONTAINER override it.
    
    Signed-off-by: Anil Thomas <anlthms@users.noreply.github.com>

--- submit_nemorl.sh
+++ submit_nemorl.sh
@@ -6,7 +6,7 @@ export QOS=${QOS:-interactive}
 # Slurm wall clock, in minutes.
 export TIMEOUT_MIN=${TIMEOUT_MIN:-240}
 
-export CONTAINER="/lustre/fs1/portfolios/llmservice/projects/llmservice_fm_text/users/tene/nemo_rl_0624.sqsh"
+export CONTAINER="${CONTAINER:-/lustre/fs1/portfolios/llmservice/projects/llmservice_fm_text/users/tene/nemo_rl_0804.sqsh}"
 export MOUNTS="/lustre:/lustre,/home:/home,/lustre/fs1/portfolios/nemotron/projects/nemotron_sw_pre/users/anthomas/RL3:/opt/nemo-rl,${HOME}:${HOME}"
 
 # Leave the command below commented out to get an interactive job.
